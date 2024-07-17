2af43d62e2c2802487ffd2982272f99c9215b323
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-07-17 15:08:29 +0200

    connect front and back for signin when admin email is kana@kana.com

--- server/app/controllers/authActions.js
+++ server/app/controllers/authActions.js
@@ -1,6 +1,3 @@
-const argon2 = require("argon2");
-const jwt = require("jsonwebtoken");
-
 // Import access to database tables
 const tables = require("../../database/tables");
 
@@ -9,50 +6,33 @@ const login = async (req, res, next) => {
     console.info(req.body);
     console.info(req.body.name);
     console.info(req.body.email);
+
     // Fetch a specific user from the database based on the provided email
     const user = await tables.user.readByEmailWithPassword(req.body.email);
     console.info(user);
-    if (user === null) {
+
+    if (user === null || user.length === 0) {
+      // If the user is not found
       res.sendStatus(422);
       return;
     }
 
-    console.info(user[0].hashed_password);
+    console.info(user[0].password);
+    console.info(user[0].email);
     console.info(req.body.password);
 
-    const verified = await argon2.verify(
-      user[0].hashed_password,
-      req.body.password
-    );
+    // Compare the provided password with the stored password (assuming plain text for simplicity)
+    if (user[0].password === req.body.password) {
+      delete user[0].password;
 
-    if (verified === true) {
-      delete user[0].hashed_password;
-
-      const token = await jwt.sign(
-        { sub: user.id, isAdmin: user.is_admin },
-        process.env.APP_SECRET,
-        {
-          expiresIn: "1h",
-        }
-      );
-
-      res.cookie("access_token", token, {
-        httpOnly: true,
-        maxAge: 3600000,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "Strict",
-        path: "/",
-      });
-
-      delete user.password;
-      delete user.id;
-
+      // Respond with the user data (excluding sensitive information)
       res
         .json({
-          user,
+          user: user[0],
         })
         .status(200);
     } else {
+      // If the password does not match
       res.sendStatus(422);
     }
   } catch (err) {
@@ -60,7 +40,7 @@ const login = async (req, res, next) => {
   }
 };
 
-// Ready to export the controller functions
+// Export the controller functions
 module.exports = {
   login,
 };
