2bb06645678a8a806e94b991d64e5ee2b60e3208
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-07-17 16:21:16 +0200

    crete page of add project as admin and add add form for project

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -32,7 +32,7 @@ const router = createBrowserRouter([
         element: <Admin />,
       },
       {
-        path: "/addpj",
+        path: "/addproject",
         element: <AddProject />,
       },
     ],
--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -1,7 +1,10 @@
+import AddProject from "./AddProject";
+
 function Admin() {
   return (
     <div>
       <p>Kana</p>
+      <AddProject />
     </div>
   );
 }
